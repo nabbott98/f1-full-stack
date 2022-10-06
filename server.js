dc509698e8f60c53f696692b98a0ab03352e9291
nabbott98 <nicholasabbott98@gmail.com>
2022-10-05 19:38:28 -0500

    Login and Logout work in postman

--- server.js
+++ server.js
@@ -3,10 +3,12 @@
 /////////////////////////////////////////////
 require("dotenv").config() // Load ENV Variables
 const express = require("express") // import express
+
 const morgan = require("morgan") // import morgan
 const path = require("path") // import path module
 const DriverRouter = require('./controllers/driverControllers')
 const UserRouter = require('./controllers/userControllers')
+const middleware = require("./utils/middleware")
 
 /////////////////////////////////////////////
 // Create our Express Application Object
@@ -17,10 +19,7 @@ const app = express()
 // Middleware
 /////////////////////////////////////////////
 // middleware runs before all the routes, every request is processed through our middleware before mongoose does anything with it.
-app.use(morgan("tiny")) // This is for request logging, the "tiny" argument declares what size of morgan log to use.
-app.use(express.urlencoded({ extended: true })) // this parses urlEncoded request bodies(useful for POST and PUT requests)
-app.use(express.static("public")) // serve files from the public folder statically
-app.use(express.json()) // parses incoming request payloads with JSON
+middleware(app)
 
 /////////////////////////////////////////////
 // Routes
